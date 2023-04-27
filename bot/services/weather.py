d96f620207368b440b355e90f90f2839f0c589d0
Nina T <123961945+Krompir12@users.noreply.github.com>
2023-04-27 15:18:59 +0200

    Update bot/services/weather.py
    added new weather URLs;
    request changed to httpx;
    try-except block changed

--- bot/services/weather.py
+++ bot/services/weather.py
@@ -1,34 +1,55 @@
 import logging
 from bot.config import config
 from aiogram import types
-import datetime
-import requests
+import httpx
+from geopy.geocoders import Nominatim
 
 logger = logging.getLogger(__name__)
 
+OPEN_WEATHER_TOKEN = config.open_weather_token
+WEATHER_STACK_TOKEN = config.weather_stack_token
+
+
+def get_coordinates(city_name):
+    geolocator = Nominatim(user_agent="my_application")
+    location = geolocator.geocode(city_name, country_codes='me')
+    return location
+
+
+def get_temperature(json_data):
+    for key, value in json_data.items():
+        if key in ['temp', 'temperature']:
+            return value
+        elif isinstance(value, dict):
+            result = get_temperature(value)
+            if result:
+                return result
+
 
 async def get_weather(message: types.Message, city: str):
+    location = get_coordinates(city)
     try:
-        r = requests.get(
-            f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={config.open_weather_token}&units=metric"
-        )
-        data = r.json()
-
-        city = data["name"]
-        current_weather = data["main"]["temp"]
-        humidity = data["main"]["humidity"]
-        wind = data["wind"]["speed"]
-        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
-        sunrise_out = sunrise_timestamp.strftime("%H:%M:%S")
-        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
-        sunset_out = sunset_timestamp.strftime("%H:%M:%S")
-
-        await message.answer(f"Погода в городе {city} по данным OpenWeather\n"
-                             f"Температура: {current_weather} C°\n"
-                             f"Влажность: {humidity}%\n"
-                             f"Ветер: {wind} м/с\n"
-                             f"Время восхода солнца: {sunrise_out}\n"
-                             f"Время захода солнца: {sunset_out}"
-                             )
-    except requests.exceptions.RequestException:
+        latitude, longitude = location.latitude, location.longitude
+    except AttributeError:
         await message.answer("Проверь название города")
+    else:
+        URLS = [('OpenWeather', 'http://api.openweathermap.org/data/2.5/weather',
+                 {'lat': latitude, 'lon': longitude, 'appid': OPEN_WEATHER_TOKEN, 'units': 'metric'}),
+                ('WeatherStack', 'http://api.weatherstack.com/current',
+                 {'access_key': WEATHER_STACK_TOKEN, 'query': city}),
+                ('OpenMeteo', 'https://api.open-meteo.com/v1/forecast',
+                 {'latitude': latitude, 'longitude': longitude, 'current_weather': 'true'})
+                ]
+
+        for name, url, params in URLS:
+            try:
+                async with httpx.AsyncClient() as client:
+                    response = await client.get(url, params=params)
+            except Exception:
+                await message.answer("Ошибка")
+            else:
+                json_data = response.json()
+                temperature = get_temperature(json_data)
+                await message.answer(f"Погода в городе {city} по данным {name}\n"
+                                     f"Температура: {temperature}C°")
+
