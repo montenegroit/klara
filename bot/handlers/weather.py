761f24d2c91f00bc01384075ecc77ce2a173d6a4
Krompir12 <fauxvotredeces@gmail.com>
2023-05-04 10:36:07 +0200

    services/weather.py, main.py, req.txt files have been modified, test_weather have been added

--- bot/handlers/weather.py
+++ bot/handlers/weather.py
@@ -6,12 +6,11 @@ from bot.services.weather import get_weather
 logger = logging.getLogger(__name__)
 router = Router()
 
+pattern = re.compile(r"^/weather (.+)$")
 
-@router.message(regexp=r'^/weather (.+)$')
+
+@router.message(commands=pattern)
 async def weather_handler(message: types.Message):
-    match = re.match(r'^/weather (.+)$', message.text)
+    match = re.match(pattern, message.text)
     city = match.group(1)
     await get_weather(message, city)
-
-
-
--- bot/services/weather.py
+++ bot/services/weather.py
@@ -1,34 +1,80 @@
 import logging
-from bot.config import config
+import asyncio
+import httpx
+
 from aiogram import types
-import datetime
-import requests
+from geopy.geocoders import Nominatim
+
+from bot.config import config
 
 logger = logging.getLogger(__name__)
 
 
+def get_coordinates(city_name: str) -> str or None:
+    geolocator = Nominatim(user_agent="my_application")
+    location = geolocator.geocode(city_name, country_codes="me")
+    return location
+
+
+def get_urls(latitude: float, longitude: float) -> list[(str, dict)]:
+    urls = [
+        (
+            "OpenWeather",
+            "http://api.openweathermap.org/data/2.5/weather",
+            {
+                "lat": latitude,
+                "lon": longitude,
+                "appid": config.open_weather_token,
+                "units": "metric",
+            },
+        ),
+        (
+            "WeatherStack",
+            "http://api.weatherstack.com/current",
+            {
+                "access_key": config.weather_stack_token,
+                "query": f"{latitude},{longitude}",
+            },
+        ),
+        (
+            "OpenMeteo",
+            "https://api.open-meteo.com/v1/forecast",
+            {"latitude": latitude, "longitude": longitude, "current_weather": "true"},
+        ),
+    ]
+    return urls
+
+
+def get_temperature(json_data: dict) -> int or float:
+    for key, value in json_data.items():
+        if key in ["temp", "temperature"]:
+            return value
+        elif isinstance(value, dict):
+            result = get_temperature(value)
+            if result:
+                return result
+
+
 async def get_weather(message: types.Message, city: str):
+    location = get_coordinates(city)
     try:
-        r = requests.get(
-            f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={config.open_weather_token}&units=metric"
-        )
-        data = r.json()
-
-        city = data["name"]
-        current_weather = data["main"]["temp"]
-        humidity = data["main"]["humidity"]
-        wind = data["wind"]["speed"]
-        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
-        sunrise_out = sunrise_timestamp.strftime("%H:%M:%S")
-        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
-        sunset_out = sunset_timestamp.strftime("%H:%M:%S")
-
-        await message.answer(f"Погода в городе {city} по данным OpenWeather\n"
-                             f"Температура: {current_weather} C°\n"
-                             f"Влажность: {humidity}%\n"
-                             f"Ветер: {wind} м/с\n"
-                             f"Время восхода солнца: {sunrise_out}\n"
-                             f"Время захода солнца: {sunset_out}"
-                             )
-    except requests.exceptions.RequestException:
+        latitude, longitude = location.latitude, location.longitude
+    except AttributeError:
         await message.answer("Проверь название города")
+    else:
+        urls = get_urls(latitude, longitude)
+        coro_list = []
+        async with httpx.AsyncClient() as client:
+            for name, url, params in urls:
+                coro_list.append((name, client.get(url, params=params)))
+            responses = await asyncio.gather(*[c[1] for c in coro_list])
+        data_res = {
+            c[0]: i.json() for i, c in zip(responses, coro_list) if i.status_code == 200
+        }
+        temperature = []
+        for name, data in data_res.items():
+            temperature.append((name, get_temperature(data)))
+        temperature_text = "\n".join(
+            f"{i}. {name}: {t} C°" for i, (name, t) in enumerate(temperature, start=1)
+        )
+        await message.answer(f"Температура в городе {city}:\n{temperature_text}")
